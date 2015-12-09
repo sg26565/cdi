e40d6740535c9513a9c5a14c7fa51b73ab2e950e
Oliver Treichel <oli@.treichels.de>
2015-12-09 21:47:23 +0100

    Fixed test log4j -> slf4j

--- mapper-cdi/src/test/java/com/bayerbbs/logging/LoggingFactoryTest.java
+++ mapper-cdi/src/test/java/com/bayerbbs/logging/LoggingFactoryTest.java
@@ -5,11 +5,11 @@ import static org.junit.Assert.assertNotNull;
 
 import javax.inject.Inject;
 
-import org.apache.log4j.Logger;
 import org.jglue.cdiunit.AdditionalClasses;
 import org.jglue.cdiunit.CdiRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 
 @RunWith(CdiRunner.class)
 @AdditionalClasses(LoggingFactory.class)
