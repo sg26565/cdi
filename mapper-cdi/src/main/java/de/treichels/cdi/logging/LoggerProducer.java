9f65f28cab50bb5d818e3ce85477025bcdae964c
Oliver Treichel <oli@treichels.de>
2016-02-10 19:57:01 +0100

    Added level to MemoryLogger annotation
    Signed-off-by: Oliver Treichel <oli@treichels.de>

--- mapper-cdi/src/main/java/de/treichels/cdi/logging/LoggerProducer.java
+++ mapper-cdi/src/main/java/de/treichels/cdi/logging/LoggerProducer.java
@@ -1,14 +1,25 @@
 package de.treichels.cdi.logging;
 
+import java.util.function.Function;
+
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.spi.StandardLevel;
 
-public class LoggerProducer {
-	private final Logger logger = LogManager.getLogger();
+import de.treichels.cdi.AbstractProducer;
+
+@ApplicationScoped
+public class LoggerProducer extends AbstractProducer {
+	private static final Logger logger = LogManager.getLogger();
+
+	@Inject
+	private Function<StandardLevel, Level> mapper;
 
 	@Produces
 	public Logger getLog4JLogger(final InjectionPoint ip) {
@@ -18,10 +29,15 @@ public class LoggerProducer {
 	}
 
 	@Produces
-	@Memory
+	@MemoryLogger
 	public Logger getMemoryLogger(final InjectionPoint ip) {
 		final Class<?> declaringClass = ip.getMember().getDeclaringClass();
 		logger.printf(Level.TRACE, "injecting memory logger into %s.%s", declaringClass.getName(), ip.getMember().getName());
-		return MemoryLoggerFactory.getInstance().getLogger(declaringClass.getName());
+		final MemoryLoggerImpl logger = (MemoryLoggerImpl) MemoryLoggerFactory.getInstance().getLogger(declaringClass.getName());
+
+		final MemoryLogger annotation = getAnnotation(ip, MemoryLogger.class);
+		logger.setLevel(annotation == null ? Level.ALL : mapper.apply(annotation.level()));
+
+		return logger;
 	}
 }
--- mapper-cdi/src/main/java/de/treichels/cdi/logging/MemoryLogger.java
+++ mapper-cdi/src/main/java/de/treichels/cdi/logging/MemoryLogger.java
@@ -9,10 +9,15 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import javax.enterprise.util.Nonbinding;
 import javax.inject.Qualifier;
+
+import org.apache.logging.log4j.spi.StandardLevel;
 
 @Qualifier
 @Retention(RUNTIME)
 @Target({ METHOD, FIELD, PARAMETER, TYPE })
-public @interface Memory {
+public @interface MemoryLogger {
+	@Nonbinding
+	public StandardLevel level() default StandardLevel.ALL;
 }
